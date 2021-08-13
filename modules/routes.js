3877976f913c11ce9147bf8fe3acfa3cc3f1a056
NihadZeidan <nihadzeidan@gmail.com>
2021-08-13 13:05:29 +0300

    pre-sell route

--- modules/routes.js
+++ modules/routes.js
@@ -72,23 +72,24 @@ router.post("/sellProduct", BearerAuth, async (req, res) => {
     let time =
       today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
+    // Get Token Object
     let token = "";
     if (req.headers.authorization) {
       token = req.headers.authorization.split(" ").pop();
     }
 
+    // To find the user from user schema based on the token object
     let returnTokenObj = JWT.verify(token, mySecret);
-
-    console.log(returnTokenObj);
-
     let myUser = await UserSchema.find({
       email: returnTokenObj.email,
     });
 
+    // To check all user products
     let doesTheUserHaveProducts = await ProductSchema.find({
       userID: myUser[0]._id,
     });
 
+    // To check if the user have this specific product
     let specificProduct = false;
     doesTheUserHaveProducts.forEach((product) => {
       if (product.productCode === barCode) {
@@ -96,8 +97,7 @@ router.post("/sellProduct", BearerAuth, async (req, res) => {
       }
     });
 
-    console.log(specificProduct);
-
+    // Checking if the user have anything in the DB and have this specific product
     if (doesTheUserHaveProducts.length && specificProduct) {
       let foundProduct = await ProductSchema.find({ productCode: barCode });
 
@@ -123,29 +123,22 @@ router.post("/sellProduct", BearerAuth, async (req, res) => {
         quantitySold: +1,
       };
 
+      // To identify the product and the index for it
       let theUser = myUser;
       let productToAdd = null;
-      theUser[0].soldProducts.forEach((product) => {
+      let index = 0;
+      theUser[0].soldProducts.forEach((product, idx) => {
         // Stringify the id because it will be as an object in the mongoDB
         if (
           JSON.stringify(product._id) === JSON.stringify(foundProduct[0]._id)
         ) {
           productToAdd = foundProduct[0];
-          return;
-        }
-      });
-
-      let index = 0;
-      theUser[0].soldProducts.forEach((product, idx) => {
-        if (
-          JSON.stringify(product._id) === JSON.stringify(foundProduct[0]._id)
-        ) {
           index = idx;
           return;
         }
       });
-      console.log(index);
 
+      // To identify the product quantity
       let quantitySoldToIncrease = -1;
       if (theUser[0].soldProducts.length) {
         quantitySoldToIncrease = parseInt(
@@ -178,11 +171,12 @@ router.post("/sellProduct", BearerAuth, async (req, res) => {
   }
 });
 
-// --------------------
+// To show all the sales in the user schema
 router.get("/selling-statement", BearerAuth, (req, res) => {
   res.json({ data: req.user.soldProducts });
 });
 
+// To show the inventory statement for a user
 router.get("/inventory-statement", BearerAuth, async (req, res) => {
   try {
     let allProductsFound = await ProductSchema.find({ userID: req.user._id });
@@ -190,6 +184,48 @@ router.get("/inventory-statement", BearerAuth, async (req, res) => {
       res.json({ data: allProductsFound });
     } else {
       res.json({ response: "No Products Found !" });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/pre-sell", BearerAuth, async (req, res) => {
+  const { barCode } = req.body;
+  try {
+    // Get Token Object
+    let token = "";
+    if (req.headers.authorization) {
+      token = req.headers.authorization.split(" ").pop();
+    }
+
+    // To find the user from user schema based on the token object
+    let returnTokenObj = JWT.verify(token, mySecret);
+    let myUser = await UserSchema.find({
+      email: returnTokenObj.email,
+    });
+
+    // To check all user products
+    let doesTheUserHaveProducts = await ProductSchema.find({
+      userID: myUser[0]._id,
+    });
+
+    // To check if the user have this specific product
+    let specificProduct = false;
+    doesTheUserHaveProducts.forEach((product) => {
+      if (product.productCode === barCode) {
+        return (specificProduct = true);
+      }
+    });
+
+    if (specificProduct && doesTheUserHaveProducts.length) {
+      let foundProduct = await ProductSchema.find({ productCode: barCode });
+
+      foundProduct.length
+        ? res.json(foundProduct)
+        : res.json({ response: "error in reading the BarCode" });
+    } else {
+      res.json({ response: "This product is not in your inventory !" });
     }
   } catch (e) {
     console.log(e);
